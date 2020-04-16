76d870d4d8b531993d9f7ca3522195d6d6fabf0c
Kousuke Ariga <kouariga@gmail.com>
2020-04-17 02:26:28 +0900

    Major update to the website

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const handleClickLanding = () => {
+  const content = document.getElementsByClassName("content")[0];
+  const landingTemplate = document.getElementById("landingTemplate");
+  const landing = document.importNode(landingTemplate.content, true);
+  content.replaceWith(landing);
+};
+
+const handleClickCompany = () => {
+  const content = document.getElementsByClassName("content")[0];
+  const companyTemplate = document.getElementById("companyTemplate");
+  const company = document.importNode(companyTemplate.content, true);
+  content.replaceWith(company);
+};
+
+window.onload = () => {
+  handleClickLanding();
+
+  document.getElementById("logo").addEventListener("click", event => {
+    handleClickLanding();
+  });
+
+  document.getElementById("projectsNav").addEventListener("click", event => {
+    console.log("projects cilcked!");
+  });
+
+  document.getElementById("companyNav").addEventListener("click", event => {
+    handleClickCompany();
+  });
+};
